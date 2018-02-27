d5836f3807bb01ac50ac9513f1795ee81a2a0a91
ArlinskiEV <ArlinskiEV@gmail.com>
2018-02-27 03:47:46 +0300

    advanced-zeros/features: min count of products

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
 
-  // function line(count = 257, base = 16) {
-  //   for (i = 0; i <= count; i++) {
-  //     console.log(`${i}:${i.toString(base)}`);
-  //   }
-  // }
-
   function findMainNumber(num) {
     let i = 2;
     let pow = [];
@@ -21,34 +15,27 @@ module.exports = function getZerosCount(number, base) {
         num /= i;
       }
     }
-    console.log(pow.join(':'));// ----------------------------------------------------------------
-    console.log("-----");// ----------------------------------------------------------------
-    console.log(// ----------------------------------------------------------------
-      pow
-      // .map((item, index) => Math.pow(index, item))
-      .map((item, index) => index)
-      .filter((item) => !!item)
-      .sort((a, b) => a - b)
-    );
-    return [
-      pow
-        // .map((item, index) => Math.pow(index, item))
-        .map((item, index) => index)
-        .filter((item) => !!item)
-        .sort((a, b) => a - b)
-        .pop(),
-      0//note
-    ];
-  }
 
-  let temp = 1;
-  let baseZero = 0;
-  let [baseMain, note] = findMainNumber(base);
-  console.log(`baseMain = ${baseMain}`);// ----------------------------------------------------------------
-  while (temp < number) {
-    temp *= baseMain;
-    baseZero += Math.trunc(number / temp);
+    return pow
+      .map((item, index) => ({
+        value: Math.pow(index, item),
+        base: index,
+        count: item,
+      }))
+      .filter((item) => item.value === item.value)
+      .sort((a, b) => a.value - b.value);
   }
-
-  return baseZero;
+  
+  return findMainNumber(base)
+    .map((item) => {
+      let temp = 1;
+      let count = 0;
+      while (temp < number) {
+        temp *= item.base;
+        count += Math.trunc(number / temp);
+      }
+      return Math.trunc(count / item.count)
+    })
+    .sort((a, b) => a - b)
+    .shift();
 }
